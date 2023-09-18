22a13620c904f3a90e295221b912acaed1498faa
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-09-18 20:30:03 +0800

    restored the files

---
+++ script.js
@@ -0,0 +1,32 @@
+const header = document.querySelector("header")
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 200)
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlists = document.querySelector('.navlists');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlists.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlists.classList.remove('open');
+}
+
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration:2050,
+    delay:200,
+    reset:true
+});
+
+sr.reveal('.hero-text', {origin:'top'});
+sr.reveal('.about-img, .service-item, .about-text',{origin:'bottom'});
+sr.reveal('.about-text h2, .text-center, .right-contact h2',{origin:'top'});
+sr.reveal('.left-contact', {origin:'left'});
+sr.reveal('.right-contact', {origin:'right'});
+sr.reveal('.end-section', {origin:'top'});
